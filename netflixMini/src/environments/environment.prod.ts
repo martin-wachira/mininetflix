2d14643f8463cbe7a1831f95aee4a6f8c4fd0ee9
martin-wachira <martinnwachira@gmail.com>
2019-10-22 19:51:33 +0300

    fixed env

--- netflixMini/src/environments/environment.prod.ts
+++ netflixMini/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBuKNJVHq057JovzyBq1tVL8uIhI7YVXUw",
+    authDomain: "netflixmini-dc522.firebaseapp.com",
+    databaseURL: "https://netflixmini-dc522.firebaseio.com",
+    projectId: "netflixmini-dc522",
+    storageBucket: "netflixmini-dc522.appspot.com",
+    messagingSenderId: "243595701311",
+    appId: "1:243595701311:web:aecbbfe1ec29a843a4dc8a",
+    measurementId: "G-Z0Z471R5PK"
+  }
 };
